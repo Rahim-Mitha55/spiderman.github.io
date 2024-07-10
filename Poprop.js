46173f491290b1d758bc3bb33a73bfb344cc1f75
Rahim-Mitha55 <75312428+Rahim-Mitha55@users.noreply.github.com>
2024-07-10 16:17:45 +0530

    SpiderWeb

---
+++ Poprop.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    window.open('popup.html', 'popupWindow', 'width=600,height=400,scrollbars=yes');
+});
---
+++ User_Input.js
@@ -0,0 +1,19 @@
+
+function myFunction(){
+let x='Yo';
+let y=document.getElementById('fname').value
+let z=',how are you doing ?';
+let msg= x + y + z;
+const output= document.getElementById('msg0utput');
+
+username=window.prompt("What's your username?");
+}
+
+
+<section>
+    <h2>sample functionK</h2>
+	<label>First name: <input type="text" id="fname"></label>
+	<input type="button" value="display message"
+	
+	<h1 style="display:none" id="msg0utput"></h1>
+</section>
